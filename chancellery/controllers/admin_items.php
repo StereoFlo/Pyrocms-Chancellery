1eea78537744669eb3f4a589afedfb6590215cbb
Evgeniy <e.bronnikov@petrovich.ru>
2017-03-30 17:44:21 +0300

    refactor

--- chancellery/controllers/admin_items.php
+++ chancellery/controllers/admin_items.php
@@ -1,70 +1,97 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Admin_items extends Admin_Controller {
+class Admin_items extends Admin_Controller
+{
 
     protected $section = 'items';
-    
+
+    /**
+     * @var string
+     */
+    private $chancelleryItemUrl = '/admin/chancellery/items';
+
+    /**
+     * Admin_items constructor.
+     */
     public function __construct()
     {
-		parent::__construct();
-		$this->load->library('form_validation');
-		$this->load->model('chancellery_m');
-		$this->lang->load('chancellery');
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model('chancellery_m');
+        $this->lang->load('chancellery');
     }
-    
-    public function index ($action = NULL, $id = NULL)
+
+    /**
+     * Index action
+     */
+    public function index()
     {
-		$pagination = create_pagination('admin/chancellery/items/index', $this->chancellery_m->get_items_count(), NULL, 4);
-		$contractors = $this->chancellery_m->get_contrators($pagination["per_page"], $pagination["current_page"]);
-		$items = $this->chancellery_m->get_items();
-	    $this->template
-	    	->title($this->module_details['name'])
-	    	->set('pagination', $pagination)
-	    	->set('contractors', $contractors)
-	    	->set('items', $items)
-	    	->build('admin/items');	
+        $pagination = create_pagination('admin/chancellery/items/index', $this->chancellery_m->get_items_count(), null, 4);
+        $contractors = $this->chancellery_m->get_contrators($pagination["per_page"], $pagination["current_page"]);
+        $items = $this->chancellery_m->get_items();
+        $this->template
+            ->title($this->module_details['name'])
+            ->set('pagination', $pagination)
+            ->set('contractors', $contractors)
+            ->set('items', $items)
+            ->build('admin/items');
     }
-    
-    public function add ()
+
+    /**
+     * Add action
+     */
+    public function add()
     {
         $contractors = $this->chancellery_m->get_contrators();
         $this->template
-        	->title($this->module_details['name'])
-        	->set('contractors', $contractors)
-        	->build('admin/item_form');
+            ->title($this->module_details['name'])
+            ->set('contractors', $contractors)
+            ->build('admin/item_form');
     }
-    
-    public function edit ($id = NULL)
+
+    /**
+     * Edit action
+     *
+     * @param null $id
+     */
+    public function edit($id = null)
     {
         $contractors = $this->chancellery_m->get_contrators();
-		$item = $this->chancellery_m->get_item($id);
-		$this->template
-			->title($this->module_details['name'])
-			->set('contractors', $contractors)
-			->set('item', $item)
-			->build('admin/item_form', $this->data);
+        $item = $this->chancellery_m->get_item($id);
+        $this->template
+            ->title($this->module_details['name'])
+            ->set('contractors', $contractors)
+            ->set('item', $item)
+            ->build('admin/item_form', $this->data);
     }
-    
-    public function delete ($id = NULL)
+
+    /**
+     * Delete action
+     *
+     * @param null $id
+     */
+    public function delete($id = null)
     {
-		$this->chancellery_m->delete_item($id);
-		$this->session->set_flashdata('success', lang('message_deleted_succesfully'));
-		redirect ('/admin/chancellery/items');
+        $this->chancellery_m->delete_item($id);
+        $this->session->set_flashdata('success', lang('message_deleted_succesfully'));
+        redirect($this->chancelleryItemUrl);
+        return;
     }
-    
-    public function save ()
+
+    /**
+     * Save action
+     */
+    public function save()
     {
-		if (isset($_POST) and isset($_POST['id']))
-		{
-		    $this->chancellery_m->update_item($_POST);
-		    $this->session->set_flashdata('success', lang('message_updated_succesfully'));
-		    redirect ('/admin/chancellery/items');
-		}
-		elseif (isset($_POST) and empty($_POST['id']))
-		{
-		    $this->chancellery_m->add_item($_POST);
-		    $this->session->set_flashdata('success', lang('message_added_succesfully'));
-		    redirect ('/admin/chancellery/items');
-		}
+        if (!empty($_POST) && isset($_POST['id'])) {
+            $this->chancellery_m->update_item($_POST);
+            $this->session->set_flashdata('success', lang('message_updated_succesfully'));
+            redirect($this->chancelleryItemUrl);
+            return;
+        }
+        $this->chancellery_m->add_item($_POST);
+        $this->session->set_flashdata('success', lang('message_added_succesfully'));
+        redirect($this->chancelleryItemUrl);
+        return;
     }
 }
